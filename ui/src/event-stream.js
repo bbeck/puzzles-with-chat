a44e9eb38784844c2370b25a21bb08c824fbb845
Brandon Beck <bmbeck@gmail.com>
2020-02-27 19:47:22 -0600

    Add a client watchdog to reconnect if no events received.

--- ui/src/event-stream.js
+++ ui/src/event-stream.js
@@ -5,10 +5,12 @@ export class EventStream {
   constructor(url) {
     this.url = url;
     this.source = null;
+    this.watchdog = null;
   }
 
   setHandler(handler) {
     this.handler = handler;
+    this.stop();
     this.start();
   }
 
@@ -17,18 +19,46 @@ export class EventStream {
       throw new Error("must set a handler before starting");
     }
 
-    if (this.source) {
-      this.source.close();
-    }
-
     const start = this.start.bind(this);
+    const stop = this.stop.bind(this);
     const source = new EventSource(this.url);
     source.onerror = () => {
-      this.source.close();
-      this.source = null;
+      stop();
       setTimeout(start, 2000);
     };
-    source.onmessage = this.handler;
+
+    // Keep track of whether or not we've received data since the last watchdog
+    // check.
+    let received = false;
+    source.onmessage = (message) => {
+      received = true;
+      this.handler(message);
+    };
     this.source = source;
+
+    // Wakeup every minute and if we haven't received a message then restart
+    // the stream.
+    this.watchdog = setInterval(() => {
+      if (received) {
+        received = false;
+        return;
+      }
+
+      console.log("message hasn't been received in last 60 seconds, restarting event stream");
+      stop();
+      setTimeout(start, 100);
+    }, 60000);
+  }
+
+  stop() {
+    if (this.watchdog !== null) {
+      clearInterval(this.watchdog);
+      this.watchdog = null;
+    }
+
+    if (this.source !== null) {
+      this.source.close();
+      this.source = null;
+    }
   }
 }
--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -5,7 +5,6 @@ module.exports = function (app) {
     '/api',
     proxy({
       target: 'http://api:5000',
-      timeout: 10000,
     })
   );
 };
