d4da2a76b4ff9c7372cc8efb01ceab40ef08c228
Brandon Beck <bmbeck@gmail.com>
2020-06-08 18:15:29 -0500

    Render acrostic grid.
    This currently just shows the grid, making it take the full width of
    the puzzle area on the page.  Clues and footer are still missing.
    
    Closes #25

--- ui/src/acrostic/view.js
+++ ui/src/acrostic/view.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "acrostic/view.css";
+import {Timer} from "../common/view";
 
 export function AcrosticView(props) {
   const state = props.state;
@@ -32,29 +33,110 @@ export function AcrosticView(props) {
   }
 
   const status = state.status;
+  const view = props.view;
 
   return (
     <div id="acrostic" className={status === "selected" || status === "paused" ? "blur" : ""}>
       <div className="puzzle">
-        <Header />
-        <Grid/>
+        <Header
+          description={puzzle.description}
+          last_start_time={state.last_start_time}
+          total_solve_duration={state.total_solve_duration}
+        />
+        <Grid puzzle={puzzle} cells={state.cells} view={view}/>
+        <Clues/>
         <Footer/>
       </div>
-      <Clues/>
     </div>
   );
 }
 
-function Header() {
-  return (<div>Header goes here</div>);
+function Header({description, last_start_time, total_solve_duration}) {
+  return (
+    <div className="header">
+      <div className="description">{description}</div>
+      <Timer
+        last_start_time={last_start_time}
+        total_solve_duration={total_solve_duration}
+      />
+    </div>
+  );
 }
 
 function Footer() {
-  return (<div>Footer goes here</div>);
+  return (<div className="footer">Footer goes here</div>);
 }
 
-function Grid() {
-  return (<div>Grid goes here</div>);
+function Grid(props) {
+  const puzzle = props.puzzle;
+  const contents = props.cells;
+  const view = props.view;
+
+  // Because we're rendering as a SVG we'll make the size of each cell fixed
+  // regardless of the width or height of the puzzle.  We'll then change the
+  // view box of the SVG to contain the complete puzzle adding padding where
+  // necessary and render it accordingly.
+
+  // The side length of each cell in pixels -- needed for calculations.  This
+  // value is arbitrary and shouldn't ever need to change.
+  const s = 100;
+
+  // Border thickness of each cell in pixels -- needed for calculations.  This
+  // value should not change without also changing the CSS rule that defines the
+  // stroke width on each cell.
+  const b = 2;
+
+  // Thickness of the border surrounding the puzzle in pixels.  Half of this
+  // value encroaches into the interior of the cells so it shouldn't be made
+  // bigger than the cell's border or else cell area is lost.
+  const B = 2 * b;
+
+  // Define the border rectangle.
+  const border = (
+    <rect x={-b / 2} y={-b / 2}
+          width={s * puzzle.cols + b} height={s * puzzle.rows + b}
+          className="border"
+    />
+  );
+
+  const cells = [];
+  for (let cy = 0; cy < puzzle.rows; cy++) {
+    for (let cx = 0; cx < puzzle.cols; cx++) {
+      const number = puzzle.cell_clue_numbers[cy][cx] || "";
+      const letter = puzzle.cell_clue_letters[cy][cx] || "";
+      const content = contents[cy][cx] || "";
+      const isBlock = puzzle.cell_blocks[cy][cx];
+      const isFilled = view === "progress" && content !== "";
+      const className = isBlock ? "cell block" : isFilled ? "cell filled" : "cell";
+      const x = cx * s;
+      const y = cy * s;
+
+      cells.push(
+        <g key={cy * puzzle.cols + cx}>
+          <rect x={x} y={y} width={s} height={s} className={className}/>
+          <text x={x} y={y} className="number">{number}</text>
+          <text x={x+s} y={y} className="letter">{letter}</text>
+          <text x={x} y={y} className="content" data-length={content.length}>
+            {view !== "progress" ? content : ""}
+          </text>
+        </g>
+      );
+    }
+  }
+
+  let minX = -b / 2 - B/2;
+  let minY = -b / 2 - B/2;
+  let width = s * puzzle.cols + b + B;
+  let height = s * puzzle.rows + b + B;
+
+  return (
+    <div className="grid">
+      <svg className="grid" viewBox={`${minX} ${minY} ${width} ${height}`}>
+        {cells}
+        {border}  {/* Add this after the cells so it's drawn on top. */}
+      </svg>
+    </div>
+  );
 }
 
 function Clues() {
