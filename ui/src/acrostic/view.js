4688dce633b9bfb2376cbf06547f7092687a4824
Brandon Beck <bmbeck@gmail.com>
2020-06-10 17:31:38 -0500

    Render the clues for acrostic puzzles.
    This change updates the view to render acrostic clues in 3 columns
    that are each independently scrollable.  This should maximize the
    full whitespace underneath the puzzle grid.  The font size of the
    clue text is controllable through a setting although the font size
    of the filled in clue letter and cell number is not currently
    controllable.
    
    Closes #26

--- ui/src/acrostic/view.js
+++ ui/src/acrostic/view.js
@@ -4,8 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "acrostic/view.css";
 import {Timer} from "../common/view";
 
-export function AcrosticView(props) {
-  const state = props.state;
+export function AcrosticView({state, settings, view}) {
   const puzzle = state && state.puzzle;
   if (!state || !puzzle) {
     return (
@@ -33,7 +32,6 @@ export function AcrosticView(props) {
   }
 
   const status = state.status;
-  const view = props.view;
 
   return (
     <div id="acrostic" className={status === "selected" || status === "paused" ? "blur" : ""}>
@@ -44,7 +42,12 @@ export function AcrosticView(props) {
           total_solve_duration={state.total_solve_duration}
         />
         <Grid puzzle={puzzle} cells={state.cells} view={view}/>
-        <Clues/>
+        <Clues
+          puzzle={puzzle}
+          cells={state.cells}
+          font_size={settings.clue_font_size}
+          view={view}
+        />
         <Footer/>
       </div>
     </div>
@@ -64,14 +67,17 @@ function Header({description, last_start_time, total_solve_duration}) {
 }
 
 function Footer() {
-  return (<div className="footer">Footer goes here</div>);
+  return (
+    <div className="footer">
+      <div>Answer a clue: <code>!Q half step</code></div>
+      <div>Partially answer a clue: <code>!M sym..ony</code></div>
+      <div>Fill grid cells: <code>!26 vast knowledge</code></div>
+      <div>Make a clue visible: <code>!show H</code></div>
+    </div>
+  );
 }
 
-function Grid(props) {
-  const puzzle = props.puzzle;
-  const contents = props.cells;
-  const view = props.view;
-
+function Grid({puzzle, cells, view}) {
   // Because we're rendering as a SVG we'll make the size of each cell fixed
   // regardless of the width or height of the puzzle.  We'll then change the
   // view box of the SVG to contain the complete puzzle adding padding where
@@ -99,19 +105,19 @@ function Grid(props) {
     />
   );
 
-  const cells = [];
+  const boxes = [];
   for (let cy = 0; cy < puzzle.rows; cy++) {
     for (let cx = 0; cx < puzzle.cols; cx++) {
       const number = puzzle.cell_clue_numbers[cy][cx] || "";
       const letter = puzzle.cell_clue_letters[cy][cx] || "";
-      const content = contents[cy][cx] || "";
+      const content = cells[cy][cx] || "";
       const isBlock = puzzle.cell_blocks[cy][cx];
       const isFilled = view === "progress" && content !== "";
       const className = isBlock ? "cell block" : isFilled ? "cell filled" : "cell";
       const x = cx * s;
       const y = cy * s;
 
-      cells.push(
+      boxes.push(
         <g key={cy * puzzle.cols + cx}>
           <rect x={x} y={y} width={s} height={s} className={className}/>
           <text x={x} y={y} className="number">{number}</text>
@@ -132,13 +138,108 @@ function Grid(props) {
   return (
     <div className="grid">
       <svg className="grid" viewBox={`${minX} ${minY} ${width} ${height}`}>
-        {cells}
+        {boxes}
         {border}  {/* Add this after the cells so it's drawn on top. */}
       </svg>
     </div>
   );
 }
 
-function Clues() {
-  return (<div>Clues go here</div>);
+function Clues({puzzle, cells, font_size, view}) {
+  // First, produce a mapping of cell number to the contents of that cell.  To
+  // do this we have to go through the cells 2d array and lookup the cell number
+  // for each position.
+  const contents = {};
+  for (let cy = 0; cy < puzzle.rows; cy++) {
+    for (let cx = 0; cx < puzzle.cols; cx++) {
+      const number = puzzle.cell_clue_numbers[cy][cx];
+      if (number === 0) {
+        continue;
+      }
+
+      contents[number] = cells[cy][cx] || "";
+    }
+  }
+
+  // Next, generate each clue row.
+  const rows = [];
+  for (const clue of Object.keys(puzzle.clues).sort()) {
+    rows.push(
+      <div className="clue-row" key={clue}>
+        <div className="clue-letter">{clue}.</div>
+        <div>
+          <div className="clue" dangerouslySetInnerHTML={{__html: puzzle.clues[clue]}}/>
+          <ClueBoxes
+            letters={puzzle.clue_numbers[clue].map(n => contents[n])}
+            numbers={puzzle.clue_numbers[clue]}
+            view={view}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  // Finally we're going to render things as three separate columns so that we
+  // can take advantage of extra whitespace on the right side of the screen.
+  // To do this we'll just split the rows up into multiple sets.
+  const split1 = Math.ceil(rows.length/3);
+  const split2 = Math.ceil(split1 + (rows.length - split1)/2);
+
+  return (
+    <div className="clues" data-font-size={font_size}>
+      <div className="clue-column">{rows.slice(0, split1)}</div>
+      <div className="clue-column">{rows.slice(split1, split2)}</div>
+      <div className="clue-column">{rows.slice(split2)}</div>
+    </div>
+  );
+}
+
+function ClueBoxes({letters, numbers, view}) {
+  // The width of each cell in pixels -- needed for calculations.  This value is
+  // arbitrary and shouldn't ever need to change.
+  const w = 100;
+
+  // How far to offset the x-coordinate in pixels for the horizontal line or
+  // progress rectangle.
+  const dx = w * 0.2;
+
+  // How far from the top of the box to offset the y-coordinate in pixels for
+  // the horizontal line.
+  const dy = w * 0.6;
+
+  // The amount of padding between cells in pixels -- needed for calculations.
+  // This value is arbitrary and shouldn't ever need to change.  We use a
+  // negative padding to get the boxes closer to each other since most of the
+  // horizontal portion of the boxes is whitespace.
+  const p = -18;
+
+  const boxes = [];
+  for (let i = 0; i < letters.length; i++) {
+    const x = i*(w+p) + p;
+
+    let content;
+    if (view !== "progress") {
+      content = (<text className="letter" x={x} y="0">{letters[i]}</text>);
+    } else {
+      const className = letters[i] !== "" ? "filled" : "";
+      content = (<rect className={className} x={x + dx} y="0" width={w - 2*dx} height={dy}/>);
+    }
+
+    boxes.push(
+      <g key={i}>
+        {content}
+        <text className="number" x={x} y="0">{numbers[i]}</text>
+        <line x1={x + dx} y1="0" x2={x + w - dx} y2="0"/>
+      </g>
+    );
+  }
+
+  const width = (w + p) * letters.length;
+  const height = w;
+
+  return (
+    <svg className="clue-boxes" viewBox={`0 0 ${width} ${height}`}>
+      {boxes}
+    </svg>
+  );
 }
