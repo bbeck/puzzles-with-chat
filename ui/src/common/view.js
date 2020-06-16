817e8a5aad1780ed60bb69d1e5f0bccd744780fb
Brandon Beck <bmbeck@gmail.com>
2020-06-16 17:31:35 -0500

    Update timer component to do less work per update.
    Previously the timer would re-parse the total solve duration and the
    last start time on every duration update.  The current version only
    does parsing when the values being parsed change and during each
    update it only computes the elapsed duration.  This will hopefully
    make things a little more performant when it comes to updating the
    duration.

--- ui/src/common/view.js
+++ ui/src/common/view.js
@@ -1,62 +1,77 @@
 import React from "react";
 
-export function Timer(props) {
-  // Parse the duration string (1h10m3s) into the total number of seconds.
-  const total_solve_duration = ((duration) => {
-    let re = /(?:(?<h>[0-9]+)h)?(?:(?<m>[0-9]+)m)?(?:(?<s>[0-9.]+)s)?/;
-    let match = re.exec(duration);
+export function Timer({total_solve_duration, last_start_time}) {
+  // Parse the duration into the total number of seconds that the solve has
+  // accumulated prior to this most recent start.
+  const prior = parseDuration(total_solve_duration);
 
-    return (parseInt(match.groups.h || 0, 10)) * 3600 +
-      (parseInt(match.groups.m || 0, 10)) * 60 +
-      Math.round(parseFloat(match.groups.s || 0));
-  })(props.total_solve_duration);
+  // Parse the last start time into the number of seconds after the epoch that
+  // we started the solving segment.  If there isn't a last start time then this
+  // will be NaN.
+  const started = Date.parse(last_start_time) / 1000;
 
-  // Convert the last start time into a timestamp as a number of seconds since
-  // the epoch.  If there isn't a last start time, then this will return NaN.
-  const last_start_time = Date.parse(props.last_start_time) / 1000;
-
-  // Given an amount of time that the solve has gone for in the past as well as
-  // time time that the current segment was started at, compute the total
-  // duration in seconds that the solve has been going for.
-  const compute = (total, start) => {
-    if (!isNaN(start)) {
-      total += new Date().getTime() / 1000 - start;
-    }
-    return total > 0 ? Math.round(total) : 0;
-  };
-
-  const [total, setTotal] = React.useState(
-    compute(total_solve_duration, last_start_time)
-  );
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      const total = compute(total_solve_duration, last_start_time);
-      setTotal(total);
-    }, 500);
-    return () => clearInterval(interval)
-  }, [total_solve_duration, last_start_time, setTotal]);
-
-  return (
-    <div className="timer">
-      <Duration total={total}/>
-    </div>
-  );
+  // Render a duration that will self-update showing the number of seconds that
+  // the solve has been progressing for.  We do this separately from this
+  // component to ensure that we don't re-parse each time the duration updates.
+  return (<Duration prior={prior} started={started}/>);
 }
 
-function Duration(props) {
-  const pad = (n) => {
-    return (n < 10) ? "0" + n : n;
-  };
+function Duration({prior, started}) {
+  const [duration, setDuration] = React.useState("");
 
-  const total = props.total;
-  const hours = Math.floor(total / 3600);
-  const minutes = pad(Math.floor(total % 3600 / 60));
-  const seconds = pad(Math.floor(total % 60));
+  // Repeatedly call a callback to recompute the number of seconds that the
+  // solve has been running for.
+  useInterval(() => {
+    const delta = !isNaN(started)
+      ? new Date().getTime() / 1000 - started
+      : 0;
 
-  return (
-    <React.Fragment>
-      {`${hours}h ${minutes}m ${seconds}s`}
-    </React.Fragment>
+    const total = Math.max(Math.round(prior + delta), 0);
+    const hours = Math.floor(total / 3600);
+    const minutes = Math.floor(total % 3600 / 60);
+    const seconds = Math.floor(total % 60);
+
+    setDuration(`${hours}h ${pad(minutes)}m ${pad(seconds)}s`);
+  }, 500);
+
+  return <div className="timer">{duration}</div>;
+}
+
+// Parse the provided duration string (e.g. 1h10m3s) into the total number of
+// seconds that the duration contains.
+function parseDuration(duration) {
+  const re = /(?:(?<h>[0-9]+)h)?(?:(?<m>[0-9]+)m)?(?:(?<s>[0-9.]+)s)?/;
+  const match = re.exec(duration);
+
+  return (parseInt(match.groups.h || 0, 10)) * 3600 +
+    (parseInt(match.groups.m || 0, 10)) * 60 +
+    Math.round(parseFloat(match.groups.s || 0));
+}
+
+// Pad a number to 2 digits.
+function pad(n) {
+  return (n < 10) ? "0" + n : n;
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = React.useRef(callback);
+
+  React.useEffect(
+    () => {
+      savedCallback.current = callback;
+    },
+    [callback]
+  );
+
+  React.useEffect(
+    () => {
+      const handler = (...args) => savedCallback.current(...args);
+
+      if (delay !== null) {
+        const id = setInterval(handler, delay);
+        return () => clearInterval(id);
+      }
+    },
+    [delay]
   );
 }
